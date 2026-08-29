294c48ceff56b118da4ad0923f9a7c6a0908add0
Vadim Godunko <vgodunko@gmail.com>
2026-08-29 22:42:42 +0400

    Bind `esp_netif_t`

--- source/espidf-netif.ads
+++ source/espidf-netif.ads
@@ -6,9 +6,18 @@
 
 package ESPIDF.NETIF is
 
+   type esp_netif_t is limited private;
+
+   type esp_netif_t_ptr is access all esp_netif_t with Convention => C;
+
    function esp_netif_init return esp_err_t
      with Import, Convention => C, External_Name => "esp_netif_init";
 
    procedure esp_netif_init;
+
+private
+
+   type esp_netif_t is limited null record with Convention => C;
+   --  Full declaration of this type is not visible via "esp_netif.h"
 
 end ESPIDF.NETIF;
