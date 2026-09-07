fa6996a534d17e723e873f6b1cb98542f2fb5e6d
Vadim Godunko <vgodunko@gmail.com>
2026-09-07 17:36:44 +0400

    Bind `esp_netif_dhcps_option`

--- source/espidf-netif.adb
+++ source/espidf-netif.adb
@@ -8,6 +8,22 @@ with ESPIDF.Ada_ESP_Check_Error;
 
 package body ESPIDF.NETIF is
 
+   ----------------------------
+   -- esp_netif_dhcps_option --
+   ----------------------------
+
+   procedure esp_netif_dhcps_option
+     (esp_netif : esp_netif_t_ptr;
+      opt_op    : esp_netif_dhcp_option_mode_t;
+      opt_id    : esp_netif_dhcp_option_id_t;
+      opt_val   : System.Address;
+      opt_len   : uint32_t) is
+   begin
+      Ada_ESP_Check_Error
+        (esp_netif_dhcps_option
+           (esp_netif, opt_op, opt_id, opt_val, opt_len));
+   end esp_netif_dhcps_option;
+
    ---------------------------
    -- esp_netif_get_ip_info --
    ---------------------------
--- source/espidf-netif.ads
+++ source/espidf-netif.ads
@@ -4,8 +4,11 @@
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 --
 
+with System;
+
 with A0B.Types.Big_Endian;
 
+with ESPIDF.C_Strings;
 with ESPIDF.Event;
 
 package ESPIDF.NETIF is
@@ -13,6 +16,32 @@ package ESPIDF.NETIF is
    type esp_netif_t is limited private;
 
    type esp_netif_t_ptr is access all esp_netif_t with Convention => C;
+
+   type esp_netif_dhcp_option_mode_t is
+     (ESP_NETIF_OP_START,
+      ESP_NETIF_OP_SET,
+      ESP_NETIF_OP_GET) with Convention => C;
+
+   type esp_netif_dhcp_option_id_t is
+     (ESP_NETIF_SUBNET_MASK,
+      ESP_NETIF_DOMAIN_NAME_SERVER,
+      ESP_NETIF_ROUTER_SOLICITATION_ADDRESS,
+      ESP_NETIF_VENDOR_SPECIFIC_INFO,
+      ESP_NETIF_REQUESTED_IP_ADDRESS,
+      ESP_NETIF_IP_ADDRESS_LEASE_TIME,
+      ESP_NETIF_IP_REQUEST_RETRY_TIME,
+      ESP_NETIF_VENDOR_CLASS_IDENTIFIER,
+      ESP_NETIF_CAPTIVEPORTAL_URI) with Convention => C;
+   for esp_netif_dhcp_option_id_t use
+     (ESP_NETIF_SUBNET_MASK                   => 1,
+      ESP_NETIF_DOMAIN_NAME_SERVER            => 6,
+      ESP_NETIF_ROUTER_SOLICITATION_ADDRESS   => 32,
+      ESP_NETIF_VENDOR_SPECIFIC_INFO          => 43,
+      ESP_NETIF_REQUESTED_IP_ADDRESS          => 50,
+      ESP_NETIF_IP_ADDRESS_LEASE_TIME         => 51,
+      ESP_NETIF_IP_REQUEST_RETRY_TIME         => 52,
+      ESP_NETIF_VENDOR_CLASS_IDENTIFIER       => 60,
+      ESP_NETIF_CAPTIVEPORTAL_URI             => 114);
 
    type esp_ip4_addr_t is record
       addr : A0B.Types.Big_Endian.Unsigned_32;
@@ -52,6 +81,21 @@ package ESPIDF.NETIF is
    procedure esp_netif_get_ip_info
      (netif   : esp_netif_t_ptr;
       ip_info : out esp_netif_ip_info_t);
+
+   function esp_netif_dhcps_option
+     (esp_netif : esp_netif_t_ptr;
+      opt_op    : esp_netif_dhcp_option_mode_t;
+      opt_id    : esp_netif_dhcp_option_id_t;
+      opt_val   : System.Address;
+      opt_len   : uint32_t) return esp_err_t
+     with Import, Convention => C, External_Name => "esp_netif_dhcps_option";
+
+   procedure esp_netif_dhcps_option
+     (esp_netif : esp_netif_t_ptr;
+      opt_op    : esp_netif_dhcp_option_mode_t;
+      opt_id    : esp_netif_dhcp_option_id_t;
+      opt_val   : System.Address;
+      opt_len   : uint32_t);
 
 private
 
